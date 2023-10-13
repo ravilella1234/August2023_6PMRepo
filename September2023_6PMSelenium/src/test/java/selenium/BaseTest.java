2c6acfaf2615ffc9af8b777fd8a9fbe00dc60cd2
ravilella1234 <masters.selenium@gmail.com>
2023-10-13 19:02:25 +0530

    Log4j

--- September2023_6PMSelenium/src/test/java/selenium/BaseTest.java
+++ September2023_6PMSelenium/src/test/java/selenium/BaseTest.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -48,6 +49,9 @@ public class BaseTest
 		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\or.properties");
 		orprop = new Properties();
 		orprop.load(fis);
+		
+		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\log4jconfig.properties");
+		PropertyConfigurator.configure(fis);
 	}
 	
 	public static void launch(String browser)
